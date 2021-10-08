881bcf00070e1d7aaf68bf7c1a297e715b1323dd
MagicCompass666 <39816381+MagicCompass666@users.noreply.github.com>
2021-10-08 11:06:47 +0800

    Update StudentHelper.cpp

--- Package/Code/StudentHelper.cpp
+++ Package/Code/StudentHelper.cpp
@@ -1,11 +1,28 @@
-#include <iostream>
-// #include <Windows.h>
+#include <Windows.h>
 
 using namespace std;
 
 int main() {
-
-    
-
-    return 0;
-}
+	
+	FreeConsole();
+	
+	while (true) {
+		
+		if (GetAsyncKeyState(VK_MBUTTON) & 0x8000) {
+			system("taskkill /t /f /im StudentMain.exe");
+		}
+		
+		if (GetAsyncKeyState(VK_HOME) & 0x8000) {
+			system("start \"\" \"C:\\Program Files (x86)\\Mythware\\HP Classroom Management System-Student (Mythware)\\StudentMain.exe\"");
+		}
+		
+		if (GetAsyncKeyState(VK_END) & 0x8000) {
+			return 0;
+		} 
+		
+		Sleep(30);
+		
+	}
+	
+	return 0;
+} 
